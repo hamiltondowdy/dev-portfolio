73777c93e2a745b93f412cf738b0c17ebf18a130
hamiltondowdy <hddowdy@ncsu.edu>
2022-04-08 23:11:58 -0500

    refactor css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "../Navbar";
+import "./style.css"
 function Header() {
 
     return (
 
 <header>
- 
+  
+
   <Navbar />
 
  
-  <div class="p-5 text-center bg-light">
-    <h1 class="mb-3">Full Stack Web Developer</h1>
+  <div class="p-5 text-center bg-light" id="hero">
+    <h1 class="mb-3" id="hero-text">Full Stack Web Developer</h1>
+    <button className="neo-button">More</button>
   </div>
+
 
 </header>
 
