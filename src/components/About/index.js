9904699605c05347657aae08bc22268d3e34503d
hamiltondowdy <hddowdy@ncsu.edu>
2022-04-18 21:48:11 -0500

    refactor styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -17,24 +17,28 @@ function About() {
             <div class="container" id="abt">
 
 <div className="row">
-                <div className="hero-wrap">
-                    <div className="boxy">
+                <div className="col-md">
+                    <div className="row" id="sklrow">
+                    <div className="col" id="sklcol">
                     <img className="gif" src={gif}  alt="gif" /><img className="pfp" src={Pfp} alt="Pfp" />
                     </div>
-                   
+                    <div className="col-md" id="skil">
+                        <h1>SKILLS</h1>
+                    <Skills />
+                    </div>
+                    </div>
+                    
               
-               </div>
-                <div className="col-md justify-content">
+            
                     <h3 className="display-7">ABOUT</h3>
                     <div className="aboot">
                         <span className="display-6">Web developer with a background in client-side product integration. Taking  experience gained from working directly with developers and program managers at Zillow to test and implement pilot programs, and leveraging my personal curiosity to continually press towards my goal of finding the most efficient way of solving problems for all stakeholders. Currently enrolled in the UT Austin Coding to gain the technical skills needed to excel both individually and collaboratively in the field of Web Development. Known as a forward-thinking problem solver, the goal with every project I engage with is to create a fluid experience that is as logical to the end user as it is to me. I am able to accomplish this goal by utilizing my skills in Javascript, Python, Bash, and AWS. </span>
                        
                         
                     </div>
+                    
                     </div>
-                    <div className="pad">
-                    <Skills />
-                    </div>
+                
                  
                 
             </div>
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,34 +1,42 @@
 
 import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Collapse } from 'react-bootstrap';
+import { ButtonGroup, ToggleButton } from 'react-bootstrap';
 import "./style.css";
 
 function Skills() {
-    const [open, setOpen] = useState(false);
+  const [checked, setChecked] = useState(false);
+  const [radioValue, setRadioValue] = useState('2');
+
+  const radios = [
+    { name: 'Front End', value: '1' },
+    { name: 'All', value: '2' },
+    { name: 'Back End', value: '3' },
+  ];
   
     return (
       <>
-        <Button
-          onClick={() => setOpen(!open)}
-          aria-controls="skillsExample"
-          className="neo-button"
-          aria-expanded={open}
-        >
-          Skills 
-        </Button>
-        <Collapse in={open}>
-          <div id="skillsExample">
-            Node <br></br>
-            Express <br></br>
-            Mysql <br></br>
-            MongoDB <br></br>
-            React <br></br>
-            HTML/css
-          </div>
-        </Collapse>
-      </>
-    );
-  }
+       
+     
+      <ButtonGroup className="mb-4" id="skilly">
+        {radios.map((radio, idx) => (
+          <ToggleButton
+            key={idx}
+            id={`radio-${idx}`}
+            type="radio"
+            variant="secondary"
+            name="radio"
+            value={radio.value}
+            checked={radioValue === radio.value}
+            onChange={(e) => setRadioValue(e.currentTarget.value)}
+          >
+            {radio.name}
+          </ToggleButton>
+        ))}
+      </ButtonGroup>
+      
+    </>
+  );
+}
   
   export default Skills;
